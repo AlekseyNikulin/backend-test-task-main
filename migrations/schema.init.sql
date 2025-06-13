272c793219b0b0eed0c3ac7ee1469ee85524fe5d
Aleksey Nikulin <lex-nik1@narod.ru>
2025-06-13 19:05:00 +0500

    start

---
+++ migrations/schema.init.sql
@@ -0,0 +1,14 @@
+create table if not exists products
+(
+    id int auto_increment primary key,
+    uuid varchar(36) not null comment 'UUID товара',
+    category varchar(255) not null comment 'Категория товара',
+    is_active tinyint default 1 not null comment 'Флаг активности',
+    name text default '' not null comment 'Тип услуги',
+    description text null comment 'Описание товара',
+    thumbnail varchar(255) null comment 'Ссылка на картинку',
+    price int not null comment 'Цена в копейках'
+)
+    comment 'Товары';
+
+create index is_category_active_idx on products (category, is_active);
